3f771af8b3777550f5a5c5d01a1fdd1d9254837e
Victoria Thorsen <victoriathorsenn@gmail.com>
2021-01-11 00:47:35 -0500

    commit tree 0f2862389068be2683d66023dc9e7ff19e7e2d31

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b 
+    @c = c 
+  end
+
+  def kind
+    validate_triangle
+    if a == b && b == c 
+      :equilateral
+    elsif a == b || b == c || c == a
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def validate_triangle
+    real = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each do |side|
+      real << false if side <= 0 
+    raise TriangleError if real.include?(false)
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+ 
 end
